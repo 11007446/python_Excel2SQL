0a5e43e971a5a02eef4244320e21e94ae3d87c3b
11007446 <11007446@qq.com>
2018-06-11 17:29:35 +0800

    20180611

--- configutil.py
+++ configutil.py
@@ -6,7 +6,7 @@ class ConfigUtil:
     def __init__(self):
         conf_ini = "./test.ini"
         self.__configObj = configobj.ConfigObj(
-            './e2s_config.ini', encoding='UTF8')
+            'D:/developer/python_workspace/python_Excel2SQL/e2s_config.ini', encoding='UTF8')
         pass
 
     def getConfigString(self, key):
